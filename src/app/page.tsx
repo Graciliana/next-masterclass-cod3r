48f2e1b54635a1aff849509762427128778e730d
Graciliana Kascher <eagraciliana@gmail.com>
2024-07-15 16:51:32 -0300

    :walking: [feat] applied flexbox concepts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,11 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Masterclass Next</h1>
-      <nav>
-          <Link href="/primeiro">Primeiro Componente</Link>
+    <div className="min-h-screen bg-gradient-to-r from-teal-950">
+      <h1 className="font-bold">Masterclass Next</h1>
+      <nav className="flex flex-col">
+        <Link href="/primeiro">Primeiro Componente</Link>
+        <Link href="/flexbox">Flexbox</Link>
       </nav>
     </div>
     
---
+++ src/components/Caixa.tsx
@@ -0,0 +1,5 @@
+export default function Caixa(props: any) {
+  return (
+    <div className="  bg-lime-500 border-2 border-zinc-500 rounded-md flex justify-center items-center"><span>{ props.texto}</span></div>
+  )
+}
